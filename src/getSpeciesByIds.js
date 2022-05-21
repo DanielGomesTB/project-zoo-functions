2494720e19b938ad15375a91270b906aad5e09b0
Daniel Gomes <danielgomesveiga@hotmail.com>
2022-05-20 22:27:08 -0300

    Requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,15 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+// function getSpeciesByIds(id) {
+//   if (id === undefined) return []
+//   return species.filter((i) => {
+//     if (i.id === id)  return i
+//   })
+// }
+
+function getSpeciesByIds(...ids) {
+  return species.filter((animal) => ids.includes(animal.id));
 }
 
 module.exports = getSpeciesByIds;
