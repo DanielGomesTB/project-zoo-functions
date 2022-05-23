daf0305b70e49362fd32edd6ded8189b70172ec5
Daniel Gomes <danielgomesveiga@hotmail.com>
2022-05-23 17:32:15 -0300

    Requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,10 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  const animais = species.find((i) => animal.includes(i.name));
+  return animais.residents.every((a) => a.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
