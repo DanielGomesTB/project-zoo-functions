cf04470ea6a2a30f9c44ccff77cdfbbd789711e9
Daniel Gomes <danielgomesveiga@hotmail.com>
2022-05-23 21:48:45 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,19 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
   // seu código aqui
-}
+  if (!animal) {
+    return species.reduce((acc, elem) => {
+      acc[elem.name] = elem.residents.length;
+      return acc;
+    }, {});
+  }
+  const { residents } = species.find((specie) => specie.name === animal.specie);
 
+  if (animal.sex) {
+    return residents.filter((resident) => resident.sex === animal.sex).length;
+  }
+  return residents.length;
+}
 module.exports = countAnimals;
