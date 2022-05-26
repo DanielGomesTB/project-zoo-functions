ef063e48d2a04552b27590b9fb0587fd08ab7f54
Daniel Gomes <danielgomesveiga@hotmail.com>
2022-05-25 23:15:57 -0300

    Arrumando requisito 9

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -3,38 +3,6 @@ const data = require('../data/zoo_data');
 
 const animais = species.map((i) => i.name);
 const weekDays = ['Sunday', 'Monday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Tuesday'];
-// const dias = {
-//   Tuesday: {
-//     officeHour: 'Open from 8am until 6pm',
-//     exhibition: [ 'lions', 'tigers', 'bears', 'penguins', 'elephants', 'giraffes' ],
-//   },
-//   Wednesday: {
-//     officeHour: 'Open from 8am until 6pm',
-//     exhibition: [ 'tigers', 'bears', 'penguins', 'otters', 'frogs', 'giraffes' ],
-//   },
-//   Thursday: {
-//     officeHour: 'Open from 10am until 8pm',
-//     exhibition: [ 'lions', 'otters', 'frogs', 'snakes', 'giraffes' ],
-//   },
-//   Friday: {
-//     officeHour: 'Open from 10am until 8pm',
-//     exhibition: [ 'tigers', 'otters', 'frogs', 'snakes', 'elephants', 'giraffes' ],
-//   },
-//   Saturday: {
-//     officeHour: 'Open from 8am until 10pm',
-//     exhibition: [
-//       'lions',  'tigers',
-//       'bears',  'penguins',
-//       'otters', 'frogs',
-//       'snakes', 'elephants',
-//     ],
-//   },
-//   Sunday: {
-//     officeHour: 'Open from 8am until 8pm',
-//     exhibition: [ 'lions', 'bears', 'penguins', 'snakes', 'elephants' ],
-//   },
-//   Monday: { 'officeHour': 'CLOSED', 'exhibition': 'The zoo will be closed!' },
-// };
 const animalDisponibility = (animalName) => species.find((i) => i.name === animalName).availability;
 
 const filterAnimals = (day) => {
